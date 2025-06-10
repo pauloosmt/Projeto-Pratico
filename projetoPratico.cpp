510c112733e4c09019dd9b46f1c47e0ec3b467ca
Paulo <107899591+pauloosmt@users.noreply.github.com>
2025-06-10 19:52:46 -0300

    Add files via upload

--- projetoPratico.cpp
+++ projetoPratico.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 #include <string>
 
-// Ultima alteracao 16:30, 10/06
-// Os itens comentados estao incompletos
+// Ultima alteracao 19:54, 10/06
+// Por Paulo;
 
 
 
@@ -57,22 +57,80 @@ struct Musicas {
 
 };
 
-void ordenarANO(int tam, int vetor_pos, Musicas vet) {
-    int menor, aux;
+void ordenarANO(int tam, int *&vetor_pos, int vet[], int anoBusca, int &ocasioes) {
+    //Primeira parte: Ordenação dos anos
+    int menor, aux; 
     for(int i = 0; i < tam -1; i++) {
         menor = i;
         for (int j = i+1; j < tam; j++) {
-            if(vet[j].ano < vet[menor].ano) {
+            if(vet[vetor_pos[j]] < vet[vetor_pos[menor]]) {
                 menor = j;
             }
         }
-        aux = i;
-        vetor_pos[i] = menor;
+        aux = vetor_pos[i];
+        vetor_pos[i] = vetor_pos[menor];
         vetor_pos[menor] = aux;
     }
-    for(int k = 0; k < tam; k++) {
-        vet[vetor_pos[k]].imprimir();
+    //Foi ordenado por Selection Sort
+
+    // Segunda parte: Fazer a busca binaria e achar todas as repeticoes
+
+    // Comeca fazendo a busca binaria padrao
+    int elementosEsquer = 0, elementosDirei = tam -1;
+    bool encontrado = false;
+    int posEncontrado = 0;
+
+    while(elementosEsquer <= elementosDirei and !encontrado) {
+        int meioVetor = (elementosDirei + elementosEsquer) / 2;
+        if(vet[vetor_pos[meioVetor]] == anoBusca) {
+            posEncontrado = meioVetor;
+            encontrado = true;
+            ocasioes++;
+        } else if (vet[vetor_pos[meioVetor]] < anoBusca) {
+            elementosEsquer = meioVetor + 1;
+        } else {
+            elementosDirei = meioVetor - 1;
+        }
+
     }
+    
+    if(!encontrado){
+        cout << "ERRO! Não foi possivel achar uma musica na lista lancada em " << anoBusca << "!!!" << endl;
+        return;
+    } 
+
+    // Agora, vamos achar todas as repeticoes, comecando pela esquerda
+    int inicio = posEncontrado;
+    while (inicio > 0 and vet[vetor_pos[inicio - 1]] == anoBusca) {
+        inicio--;
+        ocasioes++; 
+    }
+
+    // Agora, vamos achar todas as repeticoes, que esta a direita da posicao encontrada na busca binaria
+    int fim = posEncontrado;
+    while(fim < tam -1 and vet[vetor_pos[fim + 1]] == anoBusca) {
+        fim++;
+        ocasioes++;
+    }
+
+    /* Nessa parte iremos redimensionar o vetor_pos, para que ele armazene apenas as 
+    posicoes que contenham musicas que forem lançadas no ano desejado */
+
+    int *temp = new int[ocasioes]; /*A variavel ocasioes auxilia, pois ela conta quantas repeticoes 
+                                       do mesmo ano existem na lista original */
+
+    for (int i = 0; i < ocasioes and inicio <= fim; i++) {
+        temp[i] = vetor_pos[inicio];
+        inicio++;
+    } 
+    
+
+    vetor_pos = temp; // Por fim, o vetor_pos passa a apontadar para o nosso vetor temporario
+    
+
+    
+    return;
+    
 }
 
 
@@ -124,10 +182,6 @@ int main () {
     
         }
 
-        //for(int i = 0)
-
-
-        
 
 
 
@@ -143,7 +197,7 @@ int main () {
             cin >> busca;
 
             if(busca == 1) {
-                for(int i =0; i < numMsc; i++) {
+                for(int i =0 ; i < numMsc; i++) {
                     buscas(busca, musicAdd[i]);
                 }
             }
@@ -151,15 +205,41 @@ int main () {
                 int buscaAno;
                 cin >> buscaAno;
 
-                int *vetor = new int[numMsc];
-                for(int i = 0; i < numMsc; i++) vetor[i] = i;
+                //Vetores auxiliares que ajudarao na ordenacao
+                int *vetor_aux = new int[numMsc]; // Esse recebe o indice que cada musica
+                int *vetorAno = new int[numMsc]; // Esse recebe o ano de cada musica
 
-                ordenarANO(numMsc, vetor, musicAdd)
+                /*Ao fim da ordenacao, o vetorAno possuira todos os anos em ordem crescente,
+                enquanto o vetor_aux, possuira a posicao desses anos em relacao ao arquivo original
+                Portanto, os elemntos do vetor_aux sao indices para buscarmos musicas no "musicAdd"
+                de maneira ordenada*/
+                
+                
+                for(int i = 0; i < numMsc; i++) {
+                    
+                    vetor_aux[i] = i;
+                    vetorAno[i] = musicAdd[i].anoLancamento;
+            
+
+                }
+
+                
+
+                int numOCasioes = 0;
+                ordenarANO(numMsc, vetor_aux, vetorAno, buscaAno, numOCasioes);
+                
+                /*Imprimindo apenas asd musicas, cujo ano de lancamento 
+                e o mesmo do ano que o usuario digitou */
+                
+                for(int i = 0; i < numOCasioes ; i++) {
+                   musicAdd[vetor_aux[i]].imprimir();
+                } 
                 
                 
 
 
-
+                delete[] vetor_aux;
+                delete[] vetorAno;
             }
         
         
@@ -177,7 +257,7 @@ int main () {
         
 
 
-
+    
         delete[] musicAdd;
     }
     
